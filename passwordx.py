ed5b97973ce46e4d14bc511ba9981c94806461ff
alexandre chakhaia <alexandre.chaxo@gmail.com>
2023-06-15 14:06:10 +0400

    Unfinished program

--- passwordx.py
+++ passwordx.py
@@ -1,1 +1,6 @@
  password = input("Enter a password that contains numbers, lowercase and uppercase Latin letters, at least 6 characters long: ")
+if len(password) < 6:
+        print("Password must be at least 6 characters long.")
+
+    elif re.search('[0-9]', password) is None:
+        print("Password must contain at least one digit.")
